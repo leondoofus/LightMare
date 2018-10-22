bbbb6063b40b627f41669266cd32d2ed65786a4b
Thanh Luong <teenage_96@yahoo.com>
2018-10-22 12:19:28 +0200

    Lv button recuperation

--- Optics-master/optics/Assets/Systems/LevelSystem.cs
+++ Optics-master/optics/Assets/Systems/LevelSystem.cs
@@ -1,10 +1,15 @@
 ﻿using UnityEngine;
 using FYFY;
+using UnityEngine.EventSystems;
+using FYFY_plugins.PointerManager;
 
 public class LevelSystem : FSystem {
-    private Family _levelGO = FamilyManager.getFamily(new AllOfComponents(typeof(LevelSelect)));
+    private Family _levelGO = FamilyManager.getFamily(new AllOfComponents(typeof(LevelSelect)), new AllOfComponents(typeof(PointerOver)));
 
 	// Use to process your families.
 	protected override void onProcess(int familiesUpdateCount) {
+        GameObject go = _levelGO.First();
+        if (go != null)
+            Debug.Log(go.name);
 	}
 }
