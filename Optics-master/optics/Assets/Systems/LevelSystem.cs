b68a8803b7d9e38cc703c9b442cf54d6e16e5e03
Thanh Luong <teenage_96@yahoo.com>
2018-10-22 17:53:52 +0200

    LvSelectors done

--- Optics-master/optics/Assets/Systems/LevelSystem.cs
+++ Optics-master/optics/Assets/Systems/LevelSystem.cs
@@ -2,14 +2,17 @@
 using FYFY;
 using UnityEngine.EventSystems;
 using FYFY_plugins.PointerManager;
+using System;
 
 public class LevelSystem : FSystem {
     private Family _levelGO = FamilyManager.getFamily(new AllOfComponents(typeof(LevelSelect)), new AllOfComponents(typeof(PointerOver)));
+    LevelSelector LV = GameObject.Find("LevelSelector").GetComponent<LevelSelector>();
 
-	// Use to process your families.
-	protected override void onProcess(int familiesUpdateCount) {
+    // Use to process your families.
+    protected override void onProcess(int familiesUpdateCount) {
         GameObject go = _levelGO.First();
         if (go != null)
-            Debug.Log(go.name);
-	}
+            if (Input.GetMouseButton(0))
+                LV.SelectLevel(Int32.Parse(go.name[go.name.Length - 1].ToString()));
+    }
 }
